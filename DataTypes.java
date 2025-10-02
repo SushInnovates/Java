dd7fb56568a0e5d4f04256dcf16fdf56066ed161
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-02 19:50:05 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        // Primitive data types
+        byte b = 120;
+        short s = 30000;
+        int i = 123456;
+        long l = 123456789L;
+        float f = 3.14f;
+        double d = 99.99;
+        char c = 'A';
+        boolean isJavaFun = true;
+
+        // Non-primitive data types
+        String name = "Java";
+        int[] arr = {10, 20, 30};
+
+        // Printing all values
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + isJavaFun);
+        System.out.println("String: " + name);
+        System.out.println("Array element [0]: " + arr[0]);
+    }
+}

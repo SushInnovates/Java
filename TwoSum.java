c6033b1ad6df378147a1836cfa62c66bc1c31a03
Sushant Sutar <sushant.sutR@gmail.com>
2026-01-28 21:36:54 +0530

    Add TwoSum class with twoSum method
    Implement TwoSum class with a method to find indices of two numbers that add up to a target.

---
+++ TwoSum.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int needed = target - nums[i];
+
+            if (map.containsKey(needed)) {
+                return new int[] { map.get(needed), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {}; // no solution case
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}

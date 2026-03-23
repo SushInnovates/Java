02b3be5b8f1cc3d71d778471cbabb08513c72856
Sushant Sutar <sushant.sutR@gmail.com>
2026-03-23 20:47:16 +0530

    Create 03_RectangleCalculations.java

---
+++ PROGRAMS/03_RectangleCalculations.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RectangleCalculations {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length of rectangle: ");
+        double length = sc.nextDouble();
+
+        System.out.print("Enter width of rectangle: ");
+        double width = sc.nextDouble();
+
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+
+        System.out.println("Area of Rectangle: " + area);
+        System.out.println("Perimeter of Rectangle: " + perimeter);
+
+        sc.close();
+    }
+}

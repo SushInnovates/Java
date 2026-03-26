727fb07368f5f17a99643ecf63151a54e5b6aec9
Sushant Sutar <sushant.sutR@gmail.com>
2026-03-26 21:56:01 +0530

    Update 06_StudentGrade.java

--- PROGRAMS/06_StudentGrade.java
+++ PROGRAMS/06_StudentGrade.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class StudentGrade {
     public static void main(String[] args) {
-
+        // Java program to determine a student's grade based on marks using if-else conditions
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter student marks: ");

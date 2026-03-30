987c36c7dacc4ce871747fe8bf9fb3a16f78e3e3
Sushant Sutar <sushant.sutR@gmail.com>
2026-03-30 22:19:25 +0530

    Add ComparisonOperators class to demonstrate operators

---
+++ PROGRAMS/10_ComparisonOperators.java
@@ -0,0 +1,14 @@
+public class ComparisonOperators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+    }
+}

a4787a24538cdd004b5e92932a166036a39f9976
Sushant Sutar <sushant.sutR@gmail.com>
2026-03-31 22:15:29 +0530

    Update 11_LogicalOperators.java

--- PROGRAMS/11_LogicalOperators.java
+++ PROGRAMS/11_LogicalOperators.java
@@ -1,6 +1,6 @@
 public class LogicalOperators {
     public static void main(String[] args) {
-
+        // Java program demonstrating logical operators such as AND (&&), OR (||), and NOT (!)
         int a = 10;
         int b = 5;
 

a91450e11a14b341e9f53a94fabc490afd063ac8
Sushant Sutar <sushant.sutR@gmail.com>
2026-04-01 21:01:03 +0530

    Update 12_PositiveNegative.java

--- PROGRAMS/12_PositiveNegative.java
+++ PROGRAMS/12_PositiveNegative.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class PositiveNegative {
     public static void main(String[] args) {
-        // Java program to check whether a given number is positive, negative, or zero using if-else conditions
+        // Java Program to Check Whether a Given Number is Positive, Negative, or zero using if-else conditions
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");

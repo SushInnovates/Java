6e338c04f8f2df8bb68d00341125a10ac1e558f1
Sushant Sutar <sushant.sutR@gmail.com>
2026-04-03 20:53:32 +0530

    Update 13_DivisibleCheck.java

--- PROGRAMS/13_DivisibleCheck.java
+++ PROGRAMS/13_DivisibleCheck.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class DivisibleCheck {
     public static void main(String[] args) {
-        // Java program to check whether a number is divisible by another number using the modulus (%) operator
+        // Java Program to Check Whether a Number Is Divisible by another number using the modulus (%) operator
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number: ");

c450085ab51c0a75c5da96376889809c083cc759
Sushant Sutar <sushant.sutR@gmail.com>
2026-04-03 20:52:37 +0530

    Create Day14_DivisibleCheck.java

---
+++ PROGRAMS/Day14_DivisibleCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DivisibleCheck {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        System.out.print("Enter the divisor: ");
+        int divisor = sc.nextInt();
+
+        if (num % divisor == 0) {
+            System.out.println("The number is divisible.");
+        } 
+        else {
+            System.out.println("The number is not divisible.");
+        }
+
+        sc.close();
+    }
+}

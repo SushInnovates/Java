3a48bc391c6a7a8f6b76efcc3a4174c8e1cdba0a
Sushant Sutar <sushant.sutR@gmail.com>
2026-04-04 19:29:54 +0530

    Update and rename 15_EvenOdd.java to 14_EvenOdd.java

--- PROGRAMS/14_EvenOdd.java
+++ PROGRAMS/14_EvenOdd.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class EvenOdd {
     public static void main(String[] args) {
-
+        // Java program to check whether a given number is even or odd using the modulus (%) operator
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
